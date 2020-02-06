3c289362cf0feadad34d348f3c8054b63827bc6a
Tomy Tjandra <tomytjandra@gmail.com>
2020-02-06 16:12:24 +0700

    tidy up code and stopwords list

--- elang/word2vec/utils/combine_stopwords.py
+++ elang/word2vec/utils/combine_stopwords.py
@@ -1,19 +1,8 @@
 import os
-from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
 
 realpath = os.path.dirname(os.path.realpath(__file__))
 stopwords_list_path = realpath + "/stopwords-list"
 file_list = os.listdir(stopwords_list_path)
-
-def generate_sastrawi_stopwords():
-    # get Sastrawi stopwords as list
-    factory = StopWordRemoverFactory()
-    stopwords = factory.get_stop_words()
-
-    # write to txt file
-    with open(stopwords_list_path + '/sastrawi-stopwords.txt', 'w') as file:
-        for word in stopwords:
-            file.write(word + "\n")
 
 def combine_stopwords_files(combined_filename = "stopwords-id.txt"):
     # combine all txt files
@@ -30,11 +19,10 @@ def combine_stopwords_files(combined_filename = "stopwords-id.txt"):
 
     # write to new txt file
     with open(realpath + "/" + combined_filename, "w") as file:
-        for word in unique_stopwords:
-            file.write(word + "\n")
+        file.write("\n".join(unique_stopwords))
 
     #print("TOTAL", len(combined_stopwords))
     #print("UNIQUE", len(unique_stopwords))
 
-generate_sastrawi_stopwords()
-combine_stopwords_files()
+if __name__ == '__main__':
+    combine_stopwords_files()
